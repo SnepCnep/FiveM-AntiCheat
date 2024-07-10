5806ea36d2bcdeae36a8e23f0f0b68005eada171
SnepCnep <tijmenk2006@gmail.com>
2024-07-10 19:38:27 +0200

    smallllll update to update checker

--- anticheat/src/server/updater.lua
+++ anticheat/src/server/updater.lua
@@ -1,10 +1,13 @@
 -- [//[ Update Checker ]\\] --
 local getCurrentVersion = GetResourceMetadata(GetCurrentResourceName(), 'version', 0) or "1.0.0"
-local githubUrl = "https://raw.githubusercontent.com/snepcnep/anticheat/main/version.txt"
+local githubUrl = "https://raw.githubusercontent.com/snepcnep/anticheat/main/version.json"
 
 CreateThread(function()
-    PerformHttpRequest(githubUrl, function()
-    
-    
+    PerformHttpRequest(githubUrl, function(code, response, headers)
+        if code == 200 then
+            -- Need to add a clean text here :)
+        else
+            print("^4 Failed to check for updates. Error code: " .. code .. "^7")
+        end
     end, "GET", "", {["Content-Type"] = "application/json"})
 end)
