0b404b9868d2e2f794ece1ef2d63c492f522527c
SnepCnep <93768063+SnepCnep@users.noreply.github.com>
2024-09-02 14:19:29 +0200

    small typo fix.

--- FiveM-anticheat/config.lua
+++ FiveM-anticheat/config.lua
@@ -1,7 +1,7 @@
 Config = {}
-Config.Debugger = true -- [[ ~This disable the ban and kick system, only for debug purposes~ ]]
+Config.Debugger = true -- [[ ~This disable the ban and kick system, only for debug purposes~ ]] --
 Config.CheckForUpdates = true
-Config.LongLoadTime = 5 -- [[ In seconds ]]
+Config.LongLoadTime = 5 -- [[ In seconds ]] --
 
 Config.WhitelistedResource = {
     [""] = true,
@@ -17,7 +17,7 @@ Config.WhitelistedEvents = {
     ["__cfx_internal:commandFallback"] = true
 }
 
-Config.AntiBackdoor = true -- Only blockt http backdoors
+Config.AntiBackdoor = true -- Only blockt http & https backdoors
 Config.HttpFilter = true
 Config.AllowedUrls = {
     [""] = true
